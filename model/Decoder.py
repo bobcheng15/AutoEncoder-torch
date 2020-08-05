50de7a8e03c33b4043d1218f9f1ea53071531594
Robert Cheng <bobcheng15@gmail.com>
2020-08-05 17:08:04 +0800

    first version done, further testing is needed

---
+++ model/Decoder.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.dconv1 = nn.ConvTranspose2d(64, 64, 3, 1, padding=1)
+        self.bn1   = nn.BatchNorm2d(64)
+        self.dconv2 = nn.ConvTranspose2d(64, 32, 4, 2, padding=1)
+        self.bn2   = nn.BatchNorm2d(32)
+        self.dconv3 = nn.ConvTranspose2d(32, 1, 8, 4, padding=0)
+        self.bn3   = nn.BatchNorm2d(1)
+        self.relu  = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.dconv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.dconv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.dconv3(x)
+        x = self.bn3(x)
+        x = self.relu(x) 
+
+
---
+++ model/Encoder.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(None, 32, 8, 4, padding=0)
+        self.bn1   = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2, padding=1)        
+        self.bn2   = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1, padding=1)
+        self.bn3   = nn.BatchNorm2d(64)
+
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        return x 
+
+
+
